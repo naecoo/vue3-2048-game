e4d94f8c4e647fd9754688037ae458d8b5c45b4a
naecoo <ocean7hy@gmail.com>
2022-09-01 23:52:33 +0800

    refactor style to scss

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -107,7 +107,9 @@ start();
   </main>
 </template>
 
-<style lang="less">
+<style lang="scss">
+@import '../styles/style.scss';
+
 .grid-container {
   display: flex;
   flex-direction: column;
@@ -134,176 +136,12 @@ start();
       font-size: 33px;
       font-weight: bold;
       border-radius: 6px;
-
-      @media (prefers-reduced-motion: no-preference) {
-        &.cell-new {
-          animation: appear 300ms ease 100ms;
-          animation-fill-mode: backwards;
-        }
-
-        &.cell-merge {
-          animation: pop 200ms ease-in 100ms;
-          animation-fill-mode: backwards;
-        }
-      }
-
-      @keyframes appear {
-        0% {
-          opacity: 0;
-          transform: scale(0);
-        }
-
-        100% {
-          opacity: 1;
-          transform: scale(1);
-        }
-      }
-
-      @keyframes pop {
-        0% {
-          transform: scale(0);
-        }
-
-        50% {
-          transform: scale(1.2);
-        }
-
-        100% {
-          transform: scale(1);
-        }
-      }
+      background-color: #c8bbaf;
+      color: #5e554c;
+      // transition: 100ms ease-in-out;
+      // animation-fill-mode: backwards;
+      @include buildCellStyle();
     }
   }
-}
-</style>
-
-<style lang="less">
-
-// todo: 生成2-32768的样式
-@tile-color: #eee4da;
-@tile-gold-color: #edc22e;
-@tile-gold-glow-color: lighten($tile-gold-color, 15%);
-@bright-text-color: #f9f6f2;
-@base: 2;
-@exponent: 1;
-@limit: 15;
-
-each(range(@exponent, @limit), {
-  @power: pow(@base, @index);
-
-  .col-@{power} {
-
-    @gold-percent: (@index - 1) / (@limit - 1) * 100;
-    @mixed-background: mix(@tile-gold-color, @tile-color, @gold-percent);
-
-    // & when(@n >= 128) {
-    //   box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0),
-    //   inset 0 0 0 1px hsla(0, 0%, 100%, 0);
-    // }
-  }
-})
-
-
-
-.cell {
-  background: #c8bbaf;
-  color: #5e554c;
-}
-
-.cell-2,
-.cell-4 {
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0);
-}
-
-.cell-2 {
-  background: #eee4da;
-}
-
-.cell-4 {
-  background: #eee2ce;
-}
-
-.cell-8 {
-  color: #f9f6f2;
-  background: #f3b27e;
-}
-
-.cell-16 {
-  color: #f9f6f2;
-  background: #f6976b;
-}
-
-.cell-32 {
-  color: #f9f6f2;
-  background: #f77e68;
-}
-
-.cell-64 {
-  color: #f9f6f2;
-  background: #f76147;
-}
-
-.cell-128 {
-  color: #f9f6f2;
-  background: #eed590;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.151515),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.0909091);
-}
-
-.cell-256 {
-  color: #f9f6f2;
-  background: #eed384;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.20202),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.121212);
-}
-
-.cell-512 {
-  color: #f9f6f2;
-  background: #edd178;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.252525),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.151515);
-}
-
-.cell-1024 {
-  color: #f9f6f2;
-  background: #edce6b;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.30303),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.181818);
-}
-
-.cell-2048 {
-  color: #f9f6f2;
-  background: #edcc5f;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.353535),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.212121);
-}
-
-.cell-4096 {
-  color: #f9f6f2;
-  background: #edc953;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.40404),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.242424);
-}
-
-.cell-8192 {
-  color: #f9f6f2;
-  background: #edc747;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.454545),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.272727);
-}
-
-.cell-16384 {
-  color: #f9f6f2;
-  background: #edc43a;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.505051),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.30303);
-}
-
-.cell-32768 {
-  color: #f9f6f2;
-  background: #edc22e;
-  box-shadow: 0 0 30px 10px rgba(243, 215, 116, 0.555556),
-    inset 0 0 0 1px hsla(0, 0%, 100%, 0.333333);
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@src/styles/index.less'
+import '@src/styles/index.scss'
 
 createApp(App).mount('#app')
