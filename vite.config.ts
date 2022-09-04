4977c33c65573063d9c4381aaa1d34a00e0918c1
naecoo <ocean7hy@gmail.com>
2022-09-05 00:08:25 +0800

    add Github Action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-2048-game/',
   resolve: {
     alias: {
       '@src': resolve(__dirname, './src')
