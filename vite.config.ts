f58ae61b75a9b3fd4305f064e7e4d95c462f49f2
naecoo <ocean7hy@gmail.com>
2022-09-05 00:09:25 +0800

    update url base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-2048-game/',
+  base: '/vue3-2048-game/',
   resolve: {
     alias: {
       '@src': resolve(__dirname, './src')
