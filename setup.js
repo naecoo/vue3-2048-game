82b052f02aa6776a06b56be83fc2c4ae54ed68b0
naecoo <ocean7hy@gmail.com>
2022-09-26 15:48:09 +0000

    Deploying to gh-pages from @ naecoo/vue3-2048-game@96f48ffc59ce9f39dd0dca16bfde5ad65972ff5f 🚀

---
+++ setup.js
@@ -0,0 +1,13 @@
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('/sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  registerSW();
+});
