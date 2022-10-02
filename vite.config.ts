213dfec7e89db8574c153afd9582b16648e74fcd
naecoo <ocean7hy@gmail.com>
2022-10-02 23:58:13 +0800

    refactor PWA to Workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
@@ -10,5 +11,41 @@ export default defineConfig({
       '@src': resolve(__dirname, './src')
     }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      scope: '/vue3-2048-game/',
+      manifest: {
+        "name": "vue3-2048-game",
+        "short_name": "2048",
+        "description": "2048 game by Vue.js 3.x",
+        "start_url": "/vue3-2048-game/",
+        "display": "standalone",
+        "background_color": "#f78e48",
+        "theme_color": "#f78e48",
+        "prefer_related_applications": true,
+        "icons": [
+          {
+            "src": "/vue3-2048-game/icon.svg",
+            "sizes": "48x48",
+            "type": "image/svg",
+            "purpose": "any maskable"
+          },
+          {
+            "src": "/vue3-2048-game/icon.svg",
+            "sizes": "192x192",
+            "type": "image/svg",
+            "purpose": "any maskable"
+          },
+          {
+            "src": "/vue3-2048-game/icon.svg",
+            "sizes": "512x512",
+            "type": "image/svg",
+            "purpose": "any maskable"
+          }
+        ]
+      }
+    })
+  ]
 })
